5fb634324759a5d099e73d7b1061e1e04cc37798
Adriano Duarte <adrianoduartedasilva@live.com>
2023-02-25 03:43:14 +0000

    Nova versão do script, também foi adicionado uma tela de dialogo.

--- funcoes.py
+++ funcoes.py
@@ -1,25 +1,26 @@
 import random
 import requests
 import pandas as pd
+import os
 
-def gerar_jogos(modalidade, quantidade_dezenas, quantidade_jogos, conc, proximo):
+def gerar_jogos(mod, quantidade_dezenas, quantidade_jogos, conc, proximo):
 
     jogos = []
     dict_jogos = {}
     for i in range(0,quantidade_jogos):
         for a in range(1, quantidade_dezenas+1):
             while True:
-                num = random.randrange(range_jogos(modalidade)[0], range_jogos(modalidade)[1])
+                num = random.randrange(range_jogos(modal(mod))[0], range_jogos(modal(mod))[1])
                 if num not in jogos:
                     jogos.append(num)
                     break
         dict_jogos[i+1] = jogos
         jogos = []
-    dict_jogos["Valor_Jogo"] = valores(modalidade,quantidade_dezenas)
-    dict_jogos["Valor_Jogos"] = quantidade_jogos * valores(modalidade,quantidade_dezenas)
+    dict_jogos["Valor_Jogo"] = valores(modal(mod),"valores",quantidade_dezenas)
+    dict_jogos["Valor_Jogos"] = quantidade_jogos * valores(modal(mod),"valores",quantidade_dezenas)
     df_jogos = pd.DataFrame(dict_jogos)
-    df_jogos.to_string("jogos/"+"jogos_" + modalidade + "_" + str(int(conc)+proximo) + ".txt", index=False)
-    proximo_concurso(modalidade,str(int(conc)+ proximo))
+    df_jogos.to_string(f"jogos/{modal(mod)}/"+"jogos_" + modal(mod) + "_" + str(int(conc)+proximo) + ".txt", index=False)
+    proximo_concurso(mod,str(int(conc)+ proximo))
 
 def range_jogos(modalidade):
     if modalidade == "megasena":
@@ -27,54 +28,97 @@ def range_jogos(modalidade):
     elif modalidade == "lotofacil":
         return(1,25)
 
-def valores(modalidade, dezenas):
-        valores = {"megasena": {6:4.5,7:31.50,8:126,9:378,10:945,11:2079,12:4158,13:7722,14:13513.50,15:22522.50,16:36036.00,17:55692.00,18:83538.00,19:122094.00,20:174420.00},"lotofacil":{15:2.5,16:40,17:350,18:2040,19:9690,20:38760}}
-        return valores[modalidade][dezenas]
+def valores(modalidade, info, valor):
+        valores = {"megasena": {"valores":{6:4.5,7:31.50,8:126,9:378,10:945,11:2079,12:4158,13:7722,14:13513.50,15:22522.50,16:36036.00,17:55692.00,18:83538.00,19:122094.00,20:174420.00},"range_dezenas":{"minimo":6,"maximo":20}},"lotofacil":{"valores":{15:2.5,16:40,17:350,18:2040,19:9690,20:38760},"range_dezenas":{"minimo":15,"maximo":20}}}
+        return valores[modalidade][info][valor]
 
-def concurso(modalidade):
-    with open("concursos_" + modalidade + ".txt", "r+") as concurso:
+def concurso(mod):
+    with open("concursos_" + modal(mod) + ".txt", "r+") as concurso:
         conc = ""
         for i in concurso:
             if i != "\n":
                 conc = i
         return conc.replace("\n","")
 
-def proximo_concurso(modalidade, conc):
-    with open("concursos_" + modalidade + ".txt", "r+") as concurso:
+def proximo_concurso(mod, conc):
+    with open("concursos_" + modal(mod) + ".txt", "r+") as concurso:
         for i in concurso:
             pass
         concurso.write(conc+"\n")
 
-def resultado(modalidade, concurso):
+def resultado(mod, concurso):
     key = open("chave.txt", "r")
-    url = f"http://apiloterias.mksoft.com.br/v1/?sorteio={modalidade}&key="+ key.readline() +"&concurso={concurso}"
+    url = f"http://apiloterias.mksoft.com.br/v1/?sorteio={modal(mod)}&key="+ key.readline() +f"&concurso={concurso}"
     response = requests.get(url, headers={"User-Agent": "XY"})
     dezenas = response.json()["dezenas"]
     return dezenas
 
-def conferencia(modalidade, concurso):
-    df_jogos = pd.read_fwf(("jogos/"+"jogos_" + modalidade + "_" + concurso + ".txt"))
-    result = resultado(modalidade, concurso)
+def conferencia(jogo, mod):
+    df_jogos = pd.read_fwf((f"jogos/{modal(mod)}/"+jogo))
+    result = resultado(mod, jogo.split("_")[2])
     numbers_result = [int(number) for number in result]
     cont = 0
     resumo = {}
+    jog = []
     for i in df_jogos:
         for a in df_jogos[i]:
             if i != "Valor_Jogo" and i != "Valor_Jogos":
                 if a in numbers_result:
                     cont += 1
-        if modalidade == "megasena":
+                    jog.append(a)
+        if modal(mod) == "megasena":
             if cont > 3:
-                resumo["Jogo " + str(i)] = cont
+                resumo["Jogo " + str(i)] = (cont,(jog))
         else:
             if cont > 10:
-                resumo["Jogo " + str(i)] = cont
-        if modalidade == "megasena":
-            if cont == 5 or cont == 6:
-                print(f"Cuncurso {concurso} e jogo {i}, modalidade {modalidade}, acertou {cont} Dezenas!")
-        else:
-            if cont == 11 or cont == 12 or cont == 13 or cont == 14 or cont == 15:
-                print(f"Cuncurso {concurso} e jogo {i}, modalidade {modalidade}, acertou {cont} Dezenas!")
-
+                resumo["Jogo " + str(i)] = (cont,(jog))
         cont = 0
+        jog = []
     return resumo
+
+def ultimo_concurso(mod):
+    key = open("chave.txt", "r")
+    url = f"http://apiloterias.mksoft.com.br/v1/?sorteio={modal(mod)}&key="+ key.readline() +"&concurso=ultimo"
+    response = requests.get(url, headers={"User-Agent": "XY"})
+    conc = response.json()["numero_concurso"]
+    return int(conc)
+
+def modal(mod):
+    modalidade = {1:"megasena",2:"lotofacil"}
+    return modalidade[mod]
+
+def texto_dash(variavel, minimo, maximo):
+    while True:
+        try:
+            if variavel == "modalidade":
+                retorno = int(input("Escolha uma das seguintes opções:\n1 - MEGASENA\n2 - LOTOFACIL\n3 - SAIR\nMODALIDADE: "))
+                if retorno == 1 or retorno == 2 or retorno == 3:
+                    return retorno
+            elif variavel == "dezenas":
+                retorno = int(input(f"\nMínimo {minimo} dezenas e Máximo {maximo} dezenas.\nQuantas dezenas por jogo?\nDEZENAS: "))
+                if retorno >= minimo and retorno <= maximo:
+                    return retorno
+            elif variavel == "quant_jogos":
+                retorno = int(input(f"\nQuantos jogos quer que eu gere?\nJOGOS: "))
+                if retorno> 0:
+                    return retorno
+            elif variavel == "gerar_conferir":
+                retorno = int(input("\nO que deseja fazer?:\n1 - GERAR JOGOS\n2 - CONFERIR JOGOS\nMODALIDADE: "))
+                if retorno == 1 or retorno == 2:
+                    return retorno
+            elif variavel == "opcao":
+                retorno = int(input(f"\nEscolha uma opçáo entre {minimo} e {maximo}.\nOPÇÃO: "))
+                if retorno >= minimo and retorno <= maximo:
+                    return retorno
+        except:
+            pass
+
+def listar_arquivos(modalidade):
+    pasta = f"./jogos/{modal(modalidade)}"
+    arquiv = {}
+    cont = 1
+    for diretorio, subpastas, arquivos in os.walk(pasta):
+        for arquivo in arquivos:
+            arquiv[cont] = arquivo
+            cont +=1
+    return arquiv
--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import funcoes as func
 
+print("BEM VINDO!\n")
 while True:
-    
-    try:
-        modalidade = "megasena"
-        concurso = func.concurso(modalidade)
-        if concurso == "": #Se não houver jogos salvos para conferencia, gerar novo.
-            concurso = "2460" #concurso inicial
-            func.gerar_jogos(modalidade,6,100, concurso, 0)
-        resumo = func.conferencia(modalidade, concurso)
-
-        if len(resumo) == 0:
-            resumo["Acertos"] = 0
-            print(resumo)
-        if modalidade == "megasena":
-            func.gerar_jogos(modalidade,6,100, concurso, 1)
-        else:
-            func.gerar_jogos(modalidade,15,10, concurso, 1)
-
-    except Exception as er:
-        print(er)
+    modalidade = func.texto_dash("modalidade",0,0)
+    if modalidade == 3:
         break
+    fluxo = func.texto_dash("gerar_conferir",0,0)
+    concurso = func.concurso(modalidade)
+
+    if fluxo == 2:
+        print("\nEscolha um dos jogos a seguir para iniciar a conferência:\n")
+        lista_jogos = func.listar_arquivos(modalidade)
+        i = 0
+        for i in lista_jogos:
+            print(f"{i} : {lista_jogos[i]}")
+        opcao = func.texto_dash("opcao", 1, i)
+        resumo = func.conferencia(lista_jogos[opcao], modalidade) 
+        if len(resumo) == 0:
+            print("\nNão houveram acertos, desejo mais sorte da próximo vez.\n")
+            input("Pressione enter para continuar\n")
+        else:
+            print("")
+            for i in resumo:
+                print(f"O {i} = {resumo[i][1]} acertou {resumo[i][0]} dezenas.")
+            print("")
+            input("Pressione ENTER para continuar. \n")
+
+    else:
+        minimo = func.valores(func.modal(modalidade),'range_dezenas', 'minimo')
+        maximo = func.valores(func.modal(modalidade),'range_dezenas', 'maximo')
+        dezenas = func.texto_dash("dezenas", minimo, maximo)
+        quant_jogos = func.texto_dash("quant_jogos", minimo, maximo)
+        func.gerar_jogos(modalidade,dezenas,quant_jogos, concurso, 0)
+        print(f"\nForam gerados {quant_jogos} jogos na modalidade {func.modal(modalidade).upper()} para o próximo concurso {concurso}, BOA SORTE!\n")
